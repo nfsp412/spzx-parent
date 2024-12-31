00956ddb3edd47f11df1b0b9676e59b45c46e0e7
Asuka <105542778+nfsp412@users.noreply.github.com>
2024-12-31 20:41:23 +0800

    commit 1

--- spzx-service/spzx-service-product/src/main/java/com/atguigu/spzx/product/ProductApplication.java
+++ spzx-service/spzx-service-product/src/main/java/com/atguigu/spzx/product/ProductApplication.java
@@ -1,10 +1,12 @@
 package com.atguigu.spzx.product;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.atguigu.spzx.product.mapper")
 public class ProductApplication {
 
     public static void main(String[] args) {
